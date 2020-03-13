97e19be35a23d8af58525c87d483859c8c60b17c
Marcin Borowski <marbor112@gmail.com>
2020-03-13 21:10:39 +0100

    HelpfulNullPointerExceptions extended.

--- java-14/src/com/rapiddelivery/example/HelpfulNullPointerExceptions.java
+++ java-14/src/com/rapiddelivery/example/HelpfulNullPointerExceptions.java
@@ -1,18 +1,40 @@
 package com.rapiddelivery.example;
 
+import java.util.List;
+
 public class HelpfulNullPointerExceptions {
 
     public static void main(String[] args) {
-        System.out.println(new UserService().getUser(0).toString());
+        var service = new UserService();
+        System.out.println(service.getUser(0).getName().toUpperCase());
+    }
+}
+
+class UserService {
+    private final List<User> users = List.of(null, new User(1L, "John"));
+
+    User getUser(long id) {
+        return users.stream()
+                .filter(u -> u.getId() == id)
+                .findFirst()
+                .orElse(null);
+    }
+}
+
+class User {
+    private final long id;
+    private final String name;
+
+    public User(long id, String name) {
+        this.id = id;
+        this.name = name;
     }
 
-    static class UserService {
-        User [] users = {null, new User()};
-        User getUser(int id) {
-            return users[id];
-        }
+    public long getId() {
+        return id;
     }
 
-    static class User {
+    public String getName() {
+        return name;
     }
 }
